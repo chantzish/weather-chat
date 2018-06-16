2601887e379cb69d8509858d7ab8ea49542bf0e7
chantzish <chantzish@gmail.com>
2018-06-16 12:40:33 +0300

    buttons to right, code clining

--- public/main.js
+++ public/main.js
@@ -16,17 +16,38 @@ var updateList = function (){
         var d = new Date(0);
         d.setUTCSeconds(city.dt);
         var card = $("<div data-index=\""+index+"\" class=\"card city mb-3\"></div>").appendTo('.city-list');
-        var cardBody =$("<div class=\"card-body\"><div class=\"container\"><div class=\"row\"><h3 class=\"card-title\">"
-        +city.name+"</h3><button type=\"button\" class=\"btn btn-outline-danger delete-city\"><i class=\"fas fa-trash-alt\"></i></button></div></div><p class=\"card-text\"><b>temp:</b> "+city.main.temp+"°</br><b>full time:</b> "+d.toString()+
-        "</p></div>").appendTo(card);
-        var commentsList = $("<ul></ul>").appendTo(cardBody);
+        var cardBody =$("<div class=\"card-body\">"+
+                          //"<div class=\"container\">"+
+                            //"<div class=\"row\">"+
+                          "<h3 class=\"card-title\">"+
+                            city.name+
+                            "<button type=\"button\" class=\"btn btn-outline-danger float-right delete-city\">"+
+                              "<i class=\"fas fa-trash-alt\"></i>"+
+                            "</button>"+
+                          "</h3>"+
+                            //"</div>"+
+                          //"</div>"+
+                          "<p class=\"card-text\">"+
+                            "<b>temp:</b> "+city.main.temp+"°</br><b>full time:</b> "+d.toString()+
+                          "</p>"+
+                        "</div>").appendTo(card);
+        var commentsList = $("<ul class=\"list-group\"></ul>").appendTo(cardBody);
         // !city.comments || city.comments.forEach((comment, index) => commentsList.append("<li data-index=\""+index+"\">"+comment+"</li>"));
-        !city.comments ? 0 : city.comments.forEach((comment, index) => commentsList.append("<div data-index=\""+index+"\"  class=\"comment\">"+comment+
-        "<button type=\"button\" class=\"btn btn-outline-danger delete-comment\"><i class=\"fas fa-trash-alt\"></i></button></div>"));
-        cardBody.append("<form class=\"add-comment\"><div class=\"input-group\"><input type=\"text\" placeholder=\"Enter Comment\""+
-        " class=\"form-control\" required><div class=\"input-group-append\"><button type=\"submit\" class=\"btn btn-success\">Comment</button>"
-        +"</div></div></form>");
-    });
+        !city.comments ? 0 : city.comments.forEach((comment, index) => commentsList.append( "<li data-index=\""+index+"\"  class=\"list-group-item comment\">"+
+                                                                                              comment+
+                                                                                              "<button type=\"button\" class=\"btn btn-outline-danger float-right delete-comment\">"+
+                                                                                                "<i class=\"fas fa-trash-alt\"></i>"+
+                                                                                              "</button>"+
+                                                                                            "</li>"));
+        cardBody.append("<form class=\"add-comment\">"+
+                          "<div class=\"input-group\">"+
+                            "<input type=\"text\" placeholder=\"Enter Comment\""+" class=\"form-control\" required>"+
+                            "<div class=\"input-group-append\">"+
+                              "<button type=\"submit\" class=\"btn btn-success\">Comment</button>"+
+                            "</div>"+
+                          "</div>"+
+                        "</form>");
+    });// end forEach city
     //add comment function 
     $('.add-comment').on('submit', function (event) {
         event.preventDefault();
@@ -48,7 +69,7 @@ var updateList = function (){
     });
     //delete comment
     $('.delete-comment').click(function () {
-        cities[$(this).closest("div.city").data().index].comments.splice($(this).closest("div.comment").data().index, 1);
+        cities[$(this).closest("div.city").data().index].comments.splice($(this).closest("li.comment").data().index, 1);
         saveToLocalStorage();
         updateList();
     });
